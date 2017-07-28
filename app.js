7f2e7b1b2a79edb422aabb17dbb8f27088ab941d
patricewhite <patricewhite2012@gmail.com>
2017-07-28 11:44:52 -0400

    pop, peek, display working

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-const createNode = (data=null, next=null) => ({
+const createNode = (data = null, next = null) => ({
   data,
   next
 });
 
-class Stack{
-  constructor(){
+class Stack {
+  constructor() {
     this.top = null;
   }
 
-  push(data){
-    if(this.top === null){
+  push(data) {
+    if (this.top === null) {
       this.top = createNode(data);
       return;
-    }
-    else{
+    } else {
       let node = createNode(data, this.top);
       this.top = node;
     }
+  }
+
+  pop() {
+    let curr = this.top;
+    this.top = this.top.next;
+    return curr.data;
+  }
+}
+
+function peek(stack) {
+  if (stack.top === null) {
+    return null;
+  } else {
+    return stack.top.data;
+  }
+}
+
+function display(stack) {
+  let node = stack.top;
+  while (node !== null) {
+    console.log(node.data);
+    node = node.next;
   }
 }
 
@@ -25,5 +46,9 @@ const myStack = new Stack;
 
 myStack.push('hi there!');
 myStack.push('goodbye');
-
-console.log(myStack);
+myStack.push('sing');
+myStack.push('dance');
+console.log('pop', myStack.pop());
+console.log('peek', peek(myStack));
+console.log('stack', myStack);
+display(myStack);
