35a796d996625389891015a550af6a5a40c5656d
williamtwobit <william@twobit.cc>
2017-07-28 11:10:38 -0400

    push successfully implemented

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+const createNode = (data=null, next=null) => ({
+  data,
+  next
+});
+
+class Stack{
+  constructor(){
+    this.top = null;
+  }
+
+  push(data){
+    if(this.top === null){
+      this.top = createNode(data);
+      return;
+    }
+    else{
+      let node = createNode(data, this.top);
+      this.top = node;
+    }
+  }
+}
+
+///// INVOKING AND LOGGING ////
+const myStack = new Stack;
+
+myStack.push('hi there!');
+myStack.push('goodbye');
+
+console.log(myStack);
